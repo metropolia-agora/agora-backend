cad1098c537a6f571b67b8d8cc85ece7f60f39d9
András Ádám <adm.andras@gmail.com>
2021-11-21 14:16:15 +0200

    Fix eslint type override

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,4 +1,4 @@
-import { User, AnonymousUser } from '../entities';
+import { User, AnonymousUser } from '../../entities';
 
 // Override the type definitions of the express library from @types/express
 // to include a user field in the request data. This field will be provided
