70dc3d4af76a095fd6b03ab7b72fe551f1d9252a
András Ádám <adm.andras@gmail.com>
2021-11-30 22:30:34 +0200

    Migrate to mysql2 from mariadb library

--- src/repository/commentRepository.ts
+++ src/repository/commentRepository.ts
@@ -1,29 +1,29 @@
-import { db } from '../utils';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import { Comment } from '../entities';
+import { db } from '../utils';
 
 class CommentRepository {
 
-  async insert(id: string, postId: string, userId: string, content: string): Promise<void> {
-    const query = 'insert into comments(id, postId, userId, content) values (?, ?, ?, ?)';
-    await db.pool.query(query, [id, postId, userId, content]);
-  }
-
   async selectById(id: string): Promise<Comment | undefined> {
     const query = 'select * from comments where id = ?';
-    const result = await db.pool.query(query, [id]);
-    if (result[0]) return new Comment(result[0]);
+    const [rows] = await db.pool.execute<RowDataPacket[]>(query, [id]);
+    if (rows[0]) return new Comment(rows[0] as Comment);
   }
 
   async selectAllByPostId(postId: string): Promise<Comment[] | undefined> {
     const query = 'select * from comments where postId = ?';
-    const result = await db.pool.query(query, [postId]);
-    const comments = result.slice(0) as Comment[];
-    return comments.map(comment => new Comment(comment));
+    const [rows] = await db.pool.execute<RowDataPacket[]>(query, [postId]);
+    return rows.map(comment => new Comment(comment as Comment));
+  }
+
+  async insert(id: string, postId: string, userId: string, content: string): Promise<void> {
+    const query = 'insert into comments(id, postId, userId, content) values (?, ?, ?, ?)';
+    await db.pool.execute<ResultSetHeader>(query, [id, postId, userId, content]);
   }
 
   async delete(id: string): Promise<void> {
     const query = 'delete from comments where id = ?';
-    await db.pool.query(query, [id]);
+    await db.pool.execute<ResultSetHeader>(query, [id]);
   }
 
 }
--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -1,3 +1,4 @@
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import { User, UserType } from '../entities';
 import { db } from '../utils';
 
@@ -5,31 +6,31 @@ class UserRepository {
 
   async selectById(id: string): Promise<User | undefined> {
     const query = 'select * from users where id = ?';
-    const result = await db.pool.query(query, [id]);
-    if (result[0]) return new User(result[0]);
+    const [rows] = await db.pool.execute<RowDataPacket[]>(query, [id]);
+    if (rows[0]) return new User(rows[0] as User);
   }
 
   async selectByUsername(username: string): Promise<User | undefined> {
     const query = 'select * from users where username = ?';
-    const result = await db.pool.query(query, [username]);
-    if (result[0]) return new User(result[0]);
+    const [rows] = await db.pool.execute<RowDataPacket[]>(query, [username]);
+    if (rows[0]) return new User(rows[0] as User);
   }
 
   async insert(id: string, type: UserType, username: string, password: string): Promise<void> {
     const query = 'insert into users(id, type, username, password) values(?, ?, ?, ?)';
-    await db.pool.query(query, [id, type, username, password]);
+    await db.pool.execute<ResultSetHeader>(query, [id, type, username, password]);
   }
 
   async delete(id: string): Promise<void> {
     const query = 'delete from users where id = ?';
-    await db.pool.query(query, [id]);
+    await db.pool.execute<ResultSetHeader>(query, [id]);
   }
 
   async update(id: string, fields: Partial<User>): Promise<void> {
     const keys = Object.keys(fields).map(field => `${field} = ?`).join(' ');
     const values = Object.values(fields);
     const query = `update users set ${keys} where id = ?`;
-    await db.pool.query(query, [...values, id]);
+    await db.pool.execute<ResultSetHeader>(query, [...values, id]);
   }
 
 }
