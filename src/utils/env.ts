6a89efc39f62f1e15806b6d052526d9422fb459a
András Ádám <adm.andras@gmail.com>
2021-12-19 19:13:05 +0100

    Hotfix for empty env variable

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 
 const getEnvironmentVariableOrThrow = (variableName: string) => {
   const variable = process.env[variableName];
-  if (!variable) throw new Error(`Could not find environment variable ${variableName}.`);
-  return variable;
+  if (variable === undefined) throw new Error(`Could not find environment variable ${variableName}.`);
+  return variable as string;
 };
 
 export const env = {
